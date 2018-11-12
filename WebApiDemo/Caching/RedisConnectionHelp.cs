ca58e91033ddb93cb828b2b131edb0a95bfd1aab
zhubingjian <939496716@qq.com>
2018-11-12 15:31:29 +0800

    Add RedisConnectionHelp

---
+++ WebApiDemo/Caching/RedisConnectionHelp.cs
@@ -0,0 +1,153 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using WebApiDemo.Common;
+
+namespace WebApiDemo.Caching
+{
+    /// <summary>
+    /// ConnectionMultiplexer对象管理帮助类
+    /// </summary>
+    public class RedisConnectionHelp
+    {
+        //"127.0.0.1:6379,allowadmin=true
+        private static readonly string RedisConnectionString = ConfigurationManager.ConnectionStrings["RedisServer"].ConnectionString;
+        private int DbNum { get; }
+        private static readonly object Locker = new object();
+        private static ConnectionMultiplexer _instance;
+        private static readonly ConcurrentDictionary<string, ConnectionMultiplexer> ConnectionCache = new ConcurrentDictionary<string, ConnectionMultiplexer>();
+
+        public RedisConnectionHelp(int dbNum = 0) : this(dbNum, null)
+        {
+        }
+        public  RedisConnectionHelp(int _dbNum,string readWriteHosts)
+        {
+            DbNum = _dbNum;
+            _instance = string.IsNullOrWhiteSpace(readWriteHosts) ? Instance : GetConnectionMultiplexer(readWriteHosts);
+        }
+        /// <summary>
+        /// 单例获取
+        /// </summary>
+        public static ConnectionMultiplexer Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (Locker)
+                    {
+                        if (_instance == null || !_instance.IsConnected)
+                        {
+                            _instance = GetManager();
+                        }
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        /// <summary>
+        /// 缓存获取
+        /// </summary>
+        /// <param name="connectionString"></param>
+        /// <returns></returns>
+        public static ConnectionMultiplexer GetConnectionMultiplexer(string connectionString)
+        {
+            if (!ConnectionCache.ContainsKey(connectionString))
+            {
+                ConnectionCache[connectionString] = GetManager(connectionString);
+            }
+            return ConnectionCache[connectionString];
+        }
+
+        private static ConnectionMultiplexer GetManager(string connectionString = null)
+        {
+            connectionString = string.IsNullOrWhiteSpace(connectionString) ? RedisConnectionString : connectionString;
+            var connect = ConnectionMultiplexer.Connect(connectionString);
+
+            //注册如下事件
+            connect.ConnectionFailed += MuxerConnectionFailed;
+            connect.ConnectionRestored += MuxerConnectionRestored;
+            connect.ErrorMessage += MuxerErrorMessage;
+            connect.ConfigurationChanged += MuxerConfigurationChanged;
+            connect.HashSlotMoved += MuxerHashSlotMoved;
+            connect.InternalError += MuxerInternalError;
+
+            return connect;
+        }
+
+        private T Redis<T>(Func<IDatabase, T> func)
+        {
+            var database = _instance.GetDatabase(DbNum);
+            return func(database);
+        }
+
+        #region 事件
+
+        /// <summary>
+        /// 配置更改时
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void MuxerConfigurationChanged(object sender, EndPointEventArgs e)
+        {
+            Function.WriteLog($"{DateTime.Now} 配置更改：" + e.EndPoint);
+        }
+
+        /// <summary>
+        /// 发生错误时
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void MuxerErrorMessage(object sender, RedisErrorEventArgs e)
+        {
+            Function.WriteLog($"{DateTime.Now} 错误信息：" + e.Message);
+        }
+
+        /// <summary>
+        /// 重新建立连接之前的错误
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void MuxerConnectionRestored(object sender, ConnectionFailedEventArgs e)
+        {
+            Function.WriteLog($"{DateTime.Now} 重连之前发生错误：" + e.EndPoint);
+        }
+
+        /// <summary>
+        /// 连接失败 ， 如果重新连接成功你将不会收到这个通知
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void MuxerConnectionFailed(object sender, ConnectionFailedEventArgs e)
+        {
+            Function.WriteLog($"{DateTime.Now} 重新连接：Endpoint failed: " + e.EndPoint + ", " + e.FailureType + (e.Exception == null ? "" : (", " + e.Exception.Message)));
+        }
+
+        /// <summary>
+        /// 更改集群
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void MuxerHashSlotMoved(object sender, HashSlotMovedEventArgs e)
+        {
+            Function.WriteLog($"{DateTime.Now} HashSlotMoved:NewEndPoint" + e.NewEndPoint + ", OldEndPoint" + e.OldEndPoint);
+        }
+
+        /// <summary>
+        /// redis类库错误
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void MuxerInternalError(object sender, InternalErrorEventArgs e)
+        {
+            Console.WriteLine("InternalError:Message" + e.Exception.Message);
+        }
+
+        #endregion 事件
+    }
+}
---
+++ WebApiDemo/Common/Function.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace WebApiDemo.Common
+{
+    public static class Function
+    {
+        public static string WriteLog(string strList)
+        {
+            try
+            {
+                //如果传过strList无内容，直接返回，不写日志  
+                if (string.IsNullOrEmpty(strList.ToString())) return "";
+                //获取本地服务器路径  
+                string strDicPath = System.Web.HttpContext.Current.Server.MapPath("~/log/");
+                //string strDicPath =  "/log/";
+
+                //创建日志路径  
+                string strPath = strDicPath + string.Format("{0:yyyy年-MM月-dd日}", DateTime.Now) + "日志记录.txt";
+                //如果服务器路径不存在，就创建一个  
+                if (!Directory.Exists(strDicPath)) Directory.CreateDirectory(strDicPath);
+                //如果日志文件不存在，创建一个  
+                if (!File.Exists(strPath)) using (FileStream fs = File.Create(strPath)) ;
+                //读取日志文件中的信息  
+                string str = File.ReadAllText(strPath);
+                StringBuilder sb = new StringBuilder();
+                //将错误信息写入sb  
+
+                sb.Append("\r\n" + DateTime.Now.ToString() + "-----" + strList + "");
+
+                //将错误信息写入txt  
+                File.WriteAllText(strPath, sb.ToString() + "\r\n-----z-----\r\n" + str);
+                return "";
+            }
+            catch (Exception ex) { return ex.ToString(); }
+        }
+    }
+}
